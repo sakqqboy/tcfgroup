178fb3219f9b57204bf5cff8314e41f289bbfdeb
sudarat1997 <nuuta.33@gmail.com>
2023-02-10 17:55:01 +0700

    update

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,8 +8,8 @@ $this->title = 'TCFGROUP';
 ?>
 
 <div class="row">
-    <img src="<?= Yii::$app->homeUrl ?>image/TCG.png" style="width: 1927" height="950">
-    <div class="text-primary">
+    <img src="<?= Yii::$app->homeUrl ?>image/TCG.png" class="img1" style="width: 1000" height="600">
+    <div class="txt-primary">
         <h1>LET'S ACCELERATE AND</h1>
         <h1>SHAPE THE FUTURE</h1>
         <h1>TOGETHER</h1>
@@ -17,11 +17,17 @@ $this->title = 'TCFGROUP';
     </div>
 </div>
 
-<div class="col-12">
-    <div class="text-left">
-        <img src="<?= Yii::$app->homeUrl ?>image/Rectangle.png" style="width: 1000" height="600">
-        <h3><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png" class="Rectangle1"> Global Business Environment</h3>
-        <p>Browse TCG Services by contry</p>
+<div class="row">
+    <img src="<?= Yii::$app->homeUrl ?>image/Rectangle4232.png" class="img3" width="1000" height="600">
+    <div class="col-md-12">
+        <h3><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png"> Global Business Environment</h3>
+        <div class="text-left">Browse TCG Services by contry</div>
+    </div>
+</div>
+
+<br>
+<div class="row">
+    <div class="col-3">
         <p><img src="<?= Yii::$app->homeUrl ?>image/Bangladesh.png" alt="Bangladesh"> Bangladesh</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/brazil.png" alt="brazil"> Brazil</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/combodia.png" alt="combodia"> Combodia</p>
@@ -35,6 +41,8 @@ $this->title = 'TCFGROUP';
         <p><img src="<?= Yii::$app->homeUrl ?>image/japan.png" alt="japan"> Japan</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/laos.png" alt="laos"> Laos</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/malaysia.png" alt="malaysia"> Malaysia</p>
+    </div>
+    <div class="col-md-2">
         <p><img src="<?= Yii::$app->homeUrl ?>image/maxico.png" alt="mexico"> Mexico</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/mongolia.png" alt="mongolia"> Mongolia</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/combodia.png" alt="combodia"> Combodia</p>
@@ -49,30 +57,54 @@ $this->title = 'TCFGROUP';
         <p><img src="<?= Yii::$app->homeUrl ?>image/thailand.png" alt="thailand"> Thailad</p>
         <p><img src="<?= Yii::$app->homeUrl ?>image/turkey.png" alt="turkey"> Turkey</p>
     </div>
-
-    <div class="content">
-        <div class="col-lg-8  col-md-4 col-12">
-            <div class="text-left col-12">
-                <h3><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png" class="Rectangle1"> Our Professional Services</h3>
-                <br>
-                <li>Establish Company</li>
-                <br>
-                <li>Cross Border M&A Services</li>
-                <br>
-                <div class="text-left">
-                    <h3>Legal & Secretarial</h3>
-                </div>
-                <br>
-                <li>Accounts & Taxation</li>
-                <br>
-                <li>Human Resource</li>
-                <br>
-                <li>Recruitment & Placement</li>
-            </div>
-        </div>
+    <div class="col-6">
+        <img src="<?= Yii::$app->homeUrl ?>image/Group.png" style="width: 100px" height="100px">
     </div>
-    <img src="<?= Yii::$app->homeUrl ?>image/Yokhohama.png" class="image-Yokhohama" style="width: 1000" height="">
-    <div class="text-left">
+</div>
+<div class="row">
+    <div class="col-3">
+        <h4><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png"> Our Professional Services</h4>
+        <br>
+        <ul>
+            <li>Establish Company</li>
+            <br>
+            <li>Cross Border M&A Services</li>
+            <br>
+            <div class="text-left">
+                <h4>Legal & Secretarial</h4>
+            </div>
+            <br>
+            <li>Accounts & Taxation</li>
+            <br>
+            <li>Human Resource</li>
+            <br>
+            <li>Recruitment & Placement</li>
+            <br>
+        </ul>
+    </div>
+    <div class="col-1">
+        <img src="<?= Yii::$app->homeUrl ?>image/Vector10.png" style="height: 600px">
+    </div>
+    <div class="text-left col-4">
+        <br>
+        <h4>Legal & Secretarial</h4>
+        <br>
+        <p>Our consulting firm specializes in providing legal and
+            secretarial support to multinational companies. Our
+            services include - Compliance Support, Corporate
+            Governance, Document Preparation, Secretarial
+            Services, corporate laws, contract laws, labor laws,
+            intellectual property laws, and compliance with local
+            laws and regulations.</p>
+    </div>
+    <div class="col-4">
+        <img src="<?= Yii::$app->homeUrl ?>image/background.png" height="600px">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-6">
+        <img src="<?= Yii::$app->homeUrl ?>image/Yokhohamaa.png" class="img" style="width: 1000" height="775">
         <h2>Establish</h2>
         <p>Business in Japan</p>
         <p>Unlock the full potential of your business in Japan with our comprehensive
@@ -82,13 +114,118 @@ $this->title = 'TCFGROUP';
             successful establishment in the Japanese market.</p>
     </div>
 </div>
-<div class="row">
-    <div class="col-md-12">
-        <div class="text-left">
-            <img src="<?= Yii::$app->homeUrl ?>image/Boat.png" style="width: 1000" height="600">
-            <p>Our Philosophy: Contributing to Society, Driving Growth</p>
-            <br>
 
+<div class="row">
+    <div class="text-left">
+        <img src="<?= Yii::$app->homeUrl ?>image/Boat.png" style="width: 1000" height="700">
+        <div class="col-12">
+            <h5><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png"> Our Philosophy: Contributing to Society, Driving Growth</h5>
+            <br>
+            <div class="text-left">
+                <h3> ❝What we
+                    <div class="text-primary">GIVE</div>is
+                </h3>
+                <h3> What we
+                    <div class="text-primary">GET</div>❞
+                </h3>
+                <br>
+            </div>
+            <p>At Tokyo Consulting Group, our philosophy is centered around making a positive impact on society. We believe that this is the ultimate goal of any company and we strive to develop human resources to fulfill this purpose. Our management principles and philosophies are dynamic, adapting to the changing times, but our commitment to contributing to society remains steadfast. Our vision, strategies and tactics are continuously evolving to stay ahead of the curve, but never at the cost of our core values. Our company's growth is measured not just by profits, but also by our ability to fulfill our social mission. As a responsible corporate citizen, we believe in finding the appropriate size that balances our growth aspirations with our obligation to society. Join us in our journey to create a better future for all."</p>
+        </div>
+        <img src="<?= Yii::$app->homeUrl ?>image/icon1.png" style="width: 100%" height="100%">
+    </div>
+</div>
+<div class="row">
+    <div class="col-5">
+        <img src="<?= Yii::$app->homeUrl ?>image/Rectangle4239.png" class="img2" style="width: 1000" height="600">
+        <h4><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png"> About Yasunari Kuno</h4>
+        <p>Yasunari Kuno is a seasoned business professional with over two decades of experience in the industry.
+            He has a strong track record of success and is respected for his leadership skills, strategic vision, and
+            ability to drive growth and profitability. In addition to his work as the chairman and founder of TCF and
+            TCG, Yasunari Kuno is also an accomplished author, having published several books on business and
+            personal development. </p>
+        <p>As a thought leader in the business world, Yasunari Kuno is known for his innovative ideas and forward thinking approach to solving complex business challenges. He is passionate about helping others achieve
+            their full potential and has dedicated his career to helping businesses succeed and thrive.</p><br> Yasunari Kuno, CEO, TOKYO CONSULTING GROUP
+    </div>
+</div>
+<div class="row">
+    <div class="col-6">
+        <h4><img src="<?= Yii::$app->homeUrl ?>image/Rectangle1.png"> Connect Experts</h4>
+        <div class="mt-5">
+            <h3>Get Free Appointment on your Business Issues</h3>
+            <p>If you would like to discuss a project or have a question, please fill in the form and we ll get right back to you.</p>
+            <div class="col-6 mt-5">
+                <p> Do you prefer to communicate as an entity or an individual?</p>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                    <label class="form-check-label" for="inlineRadio1">Legal Entity</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                    <label class="form-check-label" for="inlineRadio2">Individual</label>
+                </div>
+                <div class="mt-5">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputCompany" class="form-label">Your Company*</label>
+                            <input type="company" class="form-control" id="exampleInputCompany" aria-describedby="company">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputName" class="form-label">Your Name*</label>
+                            <input type="Name" class="form-control" id="exampleInputName" aria-describedby="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPosition" class="form-label">Position*</label>
+                            <input type="Position" class="form-control" id="exampleInputPosition" aria-describedby="position">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail" class="form-label">Your e-mail*</label>
+                            <input type="Email" class="form-control" id="exampleInputEmail" aria-describedby="email">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="validationPhone" class="form-label">Phone Number*</label>
+                            <select class="form-select" id="validationPhone" required>
+                                <option selected disabled value="">+88</option>
+                                <option>+88</option>
+                                <option>+66</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="col-6">
+        <div class="mt-5">
+            <div class="col-4 mt-5">
+                <p></p>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputQuestions" class="form-label">Questions & Concern</label>
+                        <input type="Questions" class="form-control" id="exampleInputQuestions" aria-describedby="Questions">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exmpleiInputTextarea2" class="form-label">Details of Inquiry</label>
+                        <input type="Textarea2" class="form-control" id="exampleInputTextarea2" aria-describedby="Details">
+                    </div>
+                    <br>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">ATTACHMENT (IF ANY)</label><i class="fa fa-eye" aria-hidden="true"></i>
+                    </div>
+                    <h5>WhatsApp</h5>
+                    <br>
+                    <a href="#whatsapp"><i class="fa-fa-whatsapp" aria-hidden="true"></i>inquarrytcf</a>
+                    <br>
+                    <h5>Skype</h5>
+                    <br>
+                    <a href="#skype"><i class="fa-fa-skype" aria-hidden="true"></i>skype</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="header">
+    <h1> Get Update from
+        All over the world</h1>
 </div>

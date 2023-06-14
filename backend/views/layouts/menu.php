b3a89a47b83f8cb1a960670f104cf6dcd9883561
Ekkasit <knot.ekkasit@gmail.com>
2023-06-14 11:39:12 +0700

    update 14/6

--- backend/views/layouts/menu.php
+++ backend/views/layouts/menu.php
@@ -1,27 +1,27 @@
 <nav>
-                    <ul class="nav-menu navbar-dark bg-dark">
-                        <div class="font-menu">
-                            <i class="fa fa-bars" aria-hidden="true"></i>
-                            <span class="p-1">Menu</span>
-                        </div>
-                        <div class="font-emo">
-                            ____________
-                        </div>
-                        <li class="mt-2">
-                            <a href="<?=Yii::$app->homeUrl.'/content/default/content'?>" class="lcon-menu">
-                                <i class="fa fa-home fas" aria-hidden="true"></i>
-                                <span class="nav-items">
-                                    Content
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fa fa-table fas" aria-hidden="true"></i>
-                                <span class="nav-items">
-                                    Member
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+    <ul class="nav-menu navbar-dark bg-dark">
+        <div class="font-menu">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+            <span class="p-1">Menu</span>
+        </div>
+        <div class="font-emo">
+            ____________
+        </div>
+        <li class="mt-2">
+            <a href="<?=Yii::$app->homeUrl.'/content/default/content'?>" class="lcon-menu">
+                <i class="fa fa-home fas" aria-hidden="true"></i>
+                <span class="nav-items">
+                    Content
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa fa-table fas" aria-hidden="true"></i>
+                    <span class="nav-items">
+                        Member
+                    </span>
+            </a>
+        </li>
+    </ul>
+</nav>

efbc6db2054665c5844f007c3ece517077644719
sakqqboy <saknakhngam@hotmail.com>
2023-06-29 16:52:42 +0700

    update

--- frontend/views/site/header.php
+++ frontend/views/site/header.php
@@ -13,22 +13,22 @@ use common\helpers\Path;
         <div class="collapse navbar-collapse mt-3" id="navbarScroll">
             <ul class="navbar-nav me-auto  navbar-nav-scroll" style="margin: top 40px;padding-left: 150px;font-family:'klavika';font-size: 18px;line-height:35px;">
                 <li class="nav-item">
-                    <a href="<?= Yii::$app->homeUrl ?>site/philosophy" class="nav-link">Philosophy</a>
+                    <a href="<?= Yii::$app->homeUrl ?>philosophy" class="nav-link">Philosophy</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= Yii::$app->homeUrl ?>site/services" class="nav-link">Services</a>
+                    <a href="<?= Yii::$app->homeUrl ?>services" class="nav-link">Services</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= Yii::$app->homeUrl ?>site/people" class="nav-link">People</a>
+                    <a href="<?= Yii::$app->homeUrl ?>people" class="nav-link">People</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= Yii::$app->homeUrl ?>site/career" class="nav-link">Career & Recruitment </a>
+                    <a href="<?= Yii::$app->homeUrl ?>career" class="nav-link">Career & Recruitment </a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= Yii::$app->homeUrl ?>site/about1" class="nav-link">About us</a>
+                    <a href="<?= Yii::$app->homeUrl ?>about-us" class="nav-link">About us</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= Yii::$app->homeUrl ?>site/contact1" class="nav-link">Contact Us</a>
+                    <a href="<?= Yii::$app->homeUrl ?>contact-us" class="nav-link">Contact Us</a>
                 </li>
             </ul>
         </div>

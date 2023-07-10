73db5565b87dadd26cc7f5bde5043bc4b6505de3
Balalonnn <ball49863@gmail.com>
2023-07-10 14:01:06 +0700

    asl

--- frontend/models/tokyoconsulting/Content.php
+++ frontend/models/tokyoconsulting/Content.php
@@ -51,40 +51,47 @@ class Content extends \frontend\models\tokyoconsulting\master\ContentMaster
 
     public static function footerExplore()
     {
+        $dataexplore = [];
         $explore = Content::find()->where(["contentName" => 'Explores'])->asArray()->one();
-        $detailex = ContentDetail::find()->where(["contentId" => $explore["contentId"]])->all();
-        $contentId = $explore["contentId"];
-        $dataexplore = [
-            "title" => $explore["title"],
-            "contentId" => $contentId,
-            "detailex" => $detailex
-        ];
+        if (isset($explore) && !empty($explore)) {
+            $detailex = ContentDetail::find()->where(["contentId" => $explore["contentId"]])->all();
+            $contentId = $explore["contentId"];
+            $dataexplore = [
+                "title" => $explore["title"],
+                "contentId" => $contentId,
+                "detailex" => $detailex
+            ];
+        }
         return $dataexplore;
     }
 
     public static function footerPopular()
     {
         $popular = Content::find()->where(["contentName" => 'Popular'])->asArray()->one();
-        $detailpop = ContentDetail::find()->where(["contentId" => $popular["contentId"]])->all();
-        $contentId = $popular["contentId"];
-        $datapopular = [
-            "title" => $popular["title"],
-            "contentId" => $contentId,
-            "detailpop" => $detailpop
-        ];
+        if (isset($popular) && !empty($popular)) {
+            $detailpop = ContentDetail::find()->where(["contentId" => $popular["contentId"]])->all();
+            $contentId = $popular["contentId"];
+            $datapopular = [
+                "title" => $popular["title"],
+                "contentId" => $contentId,
+                "detailpop" => $detailpop
+            ];
+        }
         return $datapopular;
     }
 
     public static function footerLogo()
     {
         $logofooter = Content::find()->where(["contentName" => 'Logofooter'])->asArray()->one();
-        $detaillogo = ContentDetail::find()->where(["contentId" => $logofooter["contentId"]])->one();
-        $contentId = $logofooter["contentId"];
-        $datalofoot = [
-            "title" => $logofooter["title"],
-            "contentId" => $contentId,
-            "detaillogo" => $detaillogo,
-        ];
+        if (isset($logofooter) && !empty($logofooter)) {
+            $detaillogo = ContentDetail::find()->where(["contentId" => $logofooter["contentId"]])->one();
+            $contentId = $logofooter["contentId"];
+            $datalofoot = [
+                "title" => $logofooter["title"],
+                "contentId" => $contentId,
+                "detaillogo" => $detaillogo,
+            ];
+        }
         return $datalofoot;
     }
 }
--- frontend/models/tokyoconsulting/ContentBranch.php
+++ frontend/models/tokyoconsulting/ContentBranch.php
@@ -39,52 +39,60 @@ class ContentBranch extends \frontend\models\tokyoconsulting\master\ContentBranc
     public static function footerMoreAbout()
     {
         $footmore = ContentBranch::find()->where(["contentName" => 'Footermoreabout'])->asArray()->one();
-        $detailmore = ContentBranchDetail::find()->where(["contentBranchId" => $footmore["contentBranchId"]])->all();
-        $contentBranchId = $footmore["contentBranchId"];
-        $datamore = [
-            "contentBranchId" => $contentBranchId,
-            "title" => $footmore["title"],
-            "detailmore" => $detailmore
-        ];
+        if (isset($footmore) && !empty($footmore)) {
+            $detailmore = ContentBranchDetail::find()->where(["contentBranchId" => $footmore["contentBranchId"]])->all();
+            $contentBranchId = $footmore["contentBranchId"];
+            $datamore = [
+                "contentBranchId" => $contentBranchId,
+                "title" => $footmore["title"],
+                "detailmore" => $detailmore
+            ];
+        }
         return $datamore;
     }
 
     public static function footerExploreTcf()
     {
         $footexp = ContentBranch::find()->where(["contentName" => 'Footerexplore'])->asArray()->one();
-        $detailexp = ContentBranchDetail::find()->where(["contentBranchId" => $footexp["contentBranchId"]])->all();
-        $contentBranchId = $footexp["contentBranchId"];
-        $dataexplorebranch = [
-            "contentBranchId" => $contentBranchId,
-            "title" => $footexp["title"],
-            "detailexp" => $detailexp
-        ];
+        if (isset($footexp) && !empty($footexp)) {
+            $detailexp = ContentBranchDetail::find()->where(["contentBranchId" => $footexp["contentBranchId"]])->all();
+            $contentBranchId = $footexp["contentBranchId"];
+            $dataexplorebranch = [
+                "contentBranchId" => $contentBranchId,
+                "title" => $footexp["title"],
+                "detailexp" => $detailexp
+            ];
+        }
         return $dataexplorebranch;
     }
 
     public static function footerPopular()
     {
         $footpopu = ContentBranch::find()->where(["contentName" => 'Footerpopular'])->asArray()->one();
-        $detailpopu = ContentBranchDetail::find()->where(["contentBranchId" => $footpopu["contentBranchId"]])->all();
-        $contentBranchId = $footpopu["contentBranchId"];
-        $datapopu = [
-            "contentBranchId" => $contentBranchId,
-            "title" => $footpopu["title"],
-            "detailpopu" => $detailpopu
-        ];
+        if (isset($footpopu) && !empty($footpopu)) {
+            $detailpopu = ContentBranchDetail::find()->where(["contentBranchId" => $footpopu["contentBranchId"]])->all();
+            $contentBranchId = $footpopu["contentBranchId"];
+            $datapopu = [
+                "contentBranchId" => $contentBranchId,
+                "title" => $footpopu["title"],
+                "detailpopu" => $detailpopu
+            ];
+        }
         return $datapopu;
     }
 
     public static function footerCountryBranch()
     {
         $footbranch = ContentBranch::find()->where(["contentName" => 'Footerbranch'])->asArray()->one();
-        $detailbranch = ContentBranchDetail::find()->where(["contentBranchId" => $footbranch["contentBranchId"]])->all();
-        $contentBranchId = $footbranch["contentBranchId"];
-        $databranch = [
-            "contentBranchId" => $contentBranchId,
-            "title" => $footbranch["title"],
-            "detailbranch" => $detailbranch
-        ];
+        if (isset($footbranch) && !empty($footbranch)) {
+            $detailbranch = ContentBranchDetail::find()->where(["contentBranchId" => $footbranch["contentBranchId"]])->all();
+            $contentBranchId = $footbranch["contentBranchId"];
+            $databranch = [
+                "contentBranchId" => $contentBranchId,
+                "title" => $footbranch["title"],
+                "detailbranch" => $detailbranch
+            ];
+        }
         return $databranch;
     }
 }

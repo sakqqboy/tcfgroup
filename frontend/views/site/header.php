e4c963e32401569e0dcc852085e49393af2fc868
sakqqboy <saknakhngam@hotmail.com>
2023-07-12 11:00:31 +0700

    header

--- frontend/views/site/header.php
+++ frontend/views/site/header.php
@@ -12,24 +12,39 @@ use common\helpers\Path;
         </button>
         <div class="collapse navbar-collapse mt-3" id="navbarScroll">
             <ul class="navbar-nav me-auto  navbar-nav-scroll" style="margin: top 40px;padding-left: 150px;font-family:'klavika';font-size: 18px;line-height:35px;">
-                <li class="nav-item">
+                <li class="nav-item <?= Yii::$app->controller->id == 'philosophy' ? 'selected-menu' : '' ?>">
                     <a href="<?= Yii::$app->homeUrl ?>philosophy" class="nav-link">Philosophy</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?= Yii::$app->controller->id == 'service' ? 'selected-menu' : '' ?>">
                     <a href="<?= Yii::$app->homeUrl ?>service" class="nav-link">Services</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?= Yii::$app->controller->id == 'people' ? 'selected-menu' : '' ?>">
                     <a href="<?= Yii::$app->homeUrl ?>people" class="nav-link">People</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?= Yii::$app->controller->id == 'career' ? 'selected-menu' : '' ?>">
                     <a href="<?= Yii::$app->homeUrl ?>career" class="nav-link">Career & Recruitment </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?= Yii::$app->controller->id == 'about-us' ? 'selected-menu' : '' ?>">
                     <a href="<?= Yii::$app->homeUrl ?>about-us" class="nav-link">About us</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?= Yii::$app->controller->id == 'contact-us' ? 'selected-menu' : '' ?>">
                     <a href="<?= Yii::$app->homeUrl ?>contact-us" class="nav-link">Contact Us</a>
                 </li>
+                <?php
+                if (Yii::$app->user->id) {
+                ?>
+                    <li class="nav-item <?= Yii::$app->controller->action->id == 'logout' ? 'selected-menu' : '' ?>">
+                        <a href="<?= Yii::$app->homeUrl ?>site/logout" class="nav-link">Log out</a>
+                    </li>
+                <?php
+                } else { ?>
+                    <li class="nav-item <?= Yii::$app->controller->action->id == 'login' ? 'selected-menu' : '' ?>">
+                        <a href="<?= Yii::$app->homeUrl ?>site-country/login" class="nav-link">Log in</a>
+                    </li>
+                <?php
+
+                }
+                ?>
             </ul>
         </div>
     </div>

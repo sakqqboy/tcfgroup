efa78b5697bab8701800c01e8ee4c207f18b4287
Ekkasit <knot.ekkasit@gmail.com>
2023-07-17 09:34:37 +0700

    Update services.php

--- frontend/views/services/services.php
+++ frontend/views/services/services.php
@@ -76,3 +76,8 @@ $this->title = 'services';
     "userInThisBranch" => $userInThisBranch,
     "branchName" => $branchName,
 ]) ?>
+
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+</div>

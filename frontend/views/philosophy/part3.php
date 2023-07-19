730246f2937a97ce9e6afebcd8fa58e1115739a0
Balalonnn <ball49863@gmail.com>
2023-07-19 17:10:24 +0700

    sss

--- frontend/views/philosophy/part3.php
+++ frontend/views/philosophy/part3.php
@@ -11,12 +11,21 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($work as $we) :
         ?>
-        <div class="col-lg-6 col-md-6 col-12 mt-50">
-            <p class="Why-We-Work"> <?= $we['title'] ?> <img src="<?= Path::backendUrl() . $we['image'] ?>"
-                    style="width: 60px;"></p>
-            <p class="everyone"><?= $we['detail'] ?> </p>
-            <p class="YASUNARI"><?= $we['detail2'] ?></p>
-        </div>
+                <div class="col-lg-6 col-md-6 col-12 mt-50">
+                    <p class="Why-We-Work"><?= $we['title'] ?> <img src="<?= Path::backendUrl() . $we['image'] ?>" style="width: 60px;"></p>
+                    <p class="everyone"><?= $we['detail'] ?> </p>
+                    <p class="YASUNARI"><?= $we['detail2'] ?></p>
+                    <?php
+                    if ($admin == 1) { ?>
+                        <div class=" col-12 text-end edit-content">
+
+                            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $we['contentId']]) ?>" target="_blank">Edit</a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+
         <?php
                 $i++;
             endforeach;
@@ -28,28 +37,24 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($playshot1 as $shot) :
         ?>
-        <div class="col-lg-6 col-md-6  col-12 mt-50 text-center">
-            <div class="col-12">
-                <img src="<?= Path::backendUrl() . $shot['image'] ?>" class="image-screenshot1">
-            </div>
-            <div class="col-12">
-                <span class="badge bg-primary image-BG1"> <a href="#" class="no-underline4"> <i
-                            class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $shot['title'] ?></a></span>
-            </div>
-        </div>
-        <?php
+                <div class="col-lg-6 col-md-6  col-12 mt-50 text-center">
+                    <div class="col-12">
+                        <img src="<?= Path::backendUrl() . $shot['image'] ?>" class="image-screenshot1">
+                    </div>
+                    <div class="col-12">
+                        <span class="badge bg-primary image-BG1"> <a href="#" class="no-underline4"> <i class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $shot['title'] ?></a></span>
+                    </div>
+                </div>
+
+            <?php
                 $i++;
             endforeach;
         }
-        ?>
-
-        <?php
         if ($admin == 1) { ?>
-        <div class=" col-12 text-end edit-content">
-            <a class="btn btn-warning bt-line"
-                href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $shot['contentId']]) ?>"
-                target="_blank">Edit</a>
-        </div>
+            <div class=" col-12 text-end edit-content">
+
+                <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $shot['contentId']]) ?>" target="_blank">Edit</a>
+            </div>
         <?php
         }
         ?>
@@ -63,15 +68,23 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($playshot2 as $hs) :
         ?>
-        <div class="col-lg-6 col-md-6 col-12 mt-50 text-center">
-            <div class="col-12">
-                <img src="<?= Path::backendUrl() . $hs['image'] ?>" class="image-screenshot2">
-            </div>
-            <div class="col-12">
-                <span class="badge bg-primary image-BG2"> <a href="#" class="no-underline4"> <i
-                            class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $hs['title'] ?></a></span>
-            </div>
-        </div>
+                <div class="col-lg-6 col-md-6 col-12 mt-50 text-center">
+                    <div class="col-12">
+                        <img src="<?= Path::backendUrl() . $hs['image'] ?>" class="image-screenshot2">
+                    </div>
+                    <div class="col-12">
+                        <span class="badge bg-primary image-BG2"> <a href="#" class="no-underline4"> <i class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $hs['title'] ?></a></span>
+                    </div>
+                    <?php
+                    if ($admin == 1) { ?>
+                        <div class=" col-12 text-end edit-content">
+
+                            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $hs['contentId']]) ?>" target="_blank">Edit</a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
         <?php
                 $i++;
             endforeach;
@@ -83,12 +96,12 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($give as $gi) :
         ?>
-        <div class="col-lg-6 col-md-6 col-12 mt-50">
-            <p class="Why-We-Work"><img src="<?= Path::backendUrl() ?>image/icon2.png" style="width: 60px;">
-                <?= $gi['title'] ?></p>
-            <p class="People"><?= $gi['detail'] ?></p>
-            <p class="TOKYO"><?= $gi['detail2'] ?></p>
-        </div>
+                <div class="col-lg-6 col-md-6 col-12 mt-50">
+                    <p class="Why-We-Work"><img src="<?= Path::backendUrl() ?>image/icon2.png" style="width: 60px;">
+                        <?= $gi['title'] ?></p>
+                    <p class="People"><?= $gi['detail'] ?></p>
+                    <p class="TOKYO"><?= $gi['detail2'] ?></p>
+                </div>
         <?php
                 $i++;
             endforeach;
@@ -97,11 +110,9 @@ use common\models\ModelMaster;
     </div>
     <?php
     if ($admin == 1) { ?>
-    <div class=" col-12 text-end edit-content">
-        <a class="btn btn-warning bt-line"
-            href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $gi['contentId']]) ?>"
-            target="_blank">Edit</a>
-    </div>
+        <div class=" col-12 text-end edit-content">
+            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $gi['contentId']]) ?>" target="_blank">Edit</a>
+        </div>
     <?php
     }
     ?>
@@ -118,15 +129,23 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($success as $suc) :
         ?>
-        <div class="col-lg-6 col-md-6  col-12 mt-50">
-            <p class="Why-We-Work"> <?= $suc['title'] ?><img src="<?= Path::backendUrl() . $suc['image'] ?>"
-                    style="width: 60px;"></p>
-            <p class="everyone"><?= $suc['detail'] ?></p>
-            <p class="everyone"><?= $suc['detail2'] ?></p>
-            <p class="everyone"><?= $suc['detail3'] ?></p>
-            <p class="everyone"> <?= $suc['detail4'] ?></p>
-            <p class="YASUNARI"><?= $suc['detail5'] ?></p>
-        </div>
+                <div class="col-lg-6 col-md-6  col-12 mt-50">
+                    <p class="Why-We-Work"> <?= $suc['title'] ?><img src="<?= Path::backendUrl() . $suc['image'] ?>" style="width: 60px;"></p>
+                    <p class="everyone"><?= $suc['detail'] ?></p>
+                    <p class="everyone"><?= $suc['detail2'] ?></p>
+                    <p class="everyone"><?= $suc['detail3'] ?></p>
+                    <p class="everyone"> <?= $suc['detail4'] ?></p>
+                    <p class="YASUNARI"><?= $suc['detail5'] ?></p>
+                    <?php
+                    if ($admin == 1) { ?>
+                        <div class=" col-12 text-end edit-content">
+
+                            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $suc['contentId']]) ?>" target="_blank">Edit</a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
         <?php
                 $i++;
             endforeach;
@@ -138,15 +157,14 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($playshot3 as $or) :
         ?>
-        <div class="col-lg-6 col-md-6  col-12 mt-50 text-center">
-            <div class="col-12">
-                <img src="<?= Path::backendUrl() . $or['image'] ?>" class="image-screenshot1">
-            </div>
-            <div class="col-12">
-                <span class="badge bg-primary image-BG1"> <a href="#" class="no-underline4"> <i
-                            class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $or['title'] ?></a> </span>
-            </div>
-        </div>
+                <div class="col-lg-6 col-md-6  col-12 mt-50 text-center">
+                    <div class="col-12">
+                        <img src="<?= Path::backendUrl() . $or['image'] ?>" class="image-screenshot1">
+                    </div>
+                    <div class="col-12">
+                        <span class="badge bg-primary image-BG1"> <a href="#" class="no-underline4"> <i class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $or['title'] ?></a> </span>
+                    </div>
+                </div>
         <?php
                 $i++;
             endforeach;
@@ -155,11 +173,9 @@ use common\models\ModelMaster;
     </div>
     <?php
     if ($admin == 1) { ?>
-    <div class=" col-12 text-end edit-content">
-        <a class="btn btn-warning bt-line"
-            href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $or['contentId']]) ?>"
-            target="_blank">Edit</a>
-    </div>
+        <div class=" col-12 text-end edit-content">
+            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $or['contentId']]) ?>" target="_blank">Edit</a>
+        </div>
     <?php
     }
     ?>
@@ -173,15 +189,23 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($playshot4 as $ly) :
         ?>
-        <div class="col-lg-6 col-md-6 col-12  mt-50 text-center">
-            <div class="col-12">
-                <img src="<?= Path::backendUrl() . $ly['image'] ?>" class="image-screenshot2">
-            </div>
-            <div class="col-12">
-                <span class="badge bg-primary image-BG2"> <a href="#" class="no-underline4"> <i
-                            class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $ly['title'] ?></a></span>
-            </div>
-        </div>
+                <div class="col-lg-6 col-md-6 col-12  mt-50 text-center">
+                    <div class="col-12">
+                        <img src="<?= Path::backendUrl() . $ly['image'] ?>" class="image-screenshot2">
+                    </div>
+                    <div class="col-12">
+                        <span class="badge bg-primary image-BG2"> <a href="#" class="no-underline4"> <i class="fa fa-play-circle link-list" aria-hidden="true"></i> <?= $ly['title'] ?></a></span>
+                    </div>
+                    <?php
+                    if ($admin == 1) { ?>
+                        <div class=" col-12 text-end edit-content">
+
+                            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $ly['contentId']]) ?>" target="_blank">Edit</a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
         <?php
                 $i++;
             endforeach;
@@ -193,12 +217,12 @@ use common\models\ModelMaster;
             $i = 0;
             foreach ($gratitude as $ti) :
         ?>
-        <div class="col-lg-6 col-md-6 col-12 mt-50">
-            <p class="Why-We-Work"><img src="<?= Path::backendUrl() ?>image/icon4.png" style="width: 60px;">
-                <?= $ti['title'] ?></p>
-            <p class="People"><?= $ti['detail'] ?></p>
-            <p class="TOKYO"><?= $ti['detail2'] ?></p>
-        </div>
+                <div class="col-lg-6 col-md-6 col-12 mt-50">
+                    <p class="Why-We-Work"><img src="<?= Path::backendUrl() ?>image/icon4.png" style="width: 60px;">
+                        <?= $ti['title'] ?></p>
+                    <p class="People"><?= $ti['detail'] ?></p>
+                    <p class="TOKYO"><?= $ti['detail2'] ?></p>
+                </div>
         <?php
                 $i++;
             endforeach;
@@ -207,11 +231,9 @@ use common\models\ModelMaster;
     </div>
     <?php
     if ($admin == 1) { ?>
-    <div class=" col-12 text-end edit-content">
-        <a class="btn btn-warning bt-line"
-            href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $ti['contentId']]) ?>"
-            target="_blank">Edit</a>
-    </div>
+        <div class=" col-12 text-end edit-content">
+            <a class="btn btn-warning bt-line" href="<?= Path::backendUrl() . 'content/default/content-detail/' . ModelMaster::encodeParams(["contentId" => $ti['contentId']]) ?>" target="_blank">Edit</a>
+        </div>
     <?php
     }
     ?>
